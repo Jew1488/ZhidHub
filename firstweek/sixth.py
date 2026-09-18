ed52b61c02d88d71647446717f6ce9b1747a9272
Gonjubar1k <tagiriys@yandex.ru>
2026-09-18 17:53:09 +0300

    Update sixth.py

--- firstweek/sixth.py
+++ firstweek/sixth.py
@@ -1,2 +1,2 @@
 s = (str(input())).split()
-for i in range(len(s)): print(s[i] + ' ' if s.count(s[i]) == 1 else '', end = '')
+for i in range(len(s)): print(s[i] + ' ' if s.count(s[i]) == 1 else '',  end = '')
