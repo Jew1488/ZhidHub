47e3ce91dd940f770bfa6f36b0c53cfff205cd01
Gonjubar1k <tagiriys@yandex.ru>
2026-09-18 17:25:03 +0300

    and sucking fxc1n balls

---
+++ firstweek/ninth.py
@@ -0,0 +1,11 @@
+f1 = open('input.txt', 'r')
+f2 = f1.readlines()
+f1.close()
+ans = 1
+s = str('')
+for i in range(len(f2)):
+    s += f2[i]
+for i in range(len(s) - 1):
+    if((s[i] == '.' or s[i] == '?' or s[i] == '!') and (s[i+1] == ' ' or s[i+1] == '\n')):
+        ans += 1
+print(ans)
